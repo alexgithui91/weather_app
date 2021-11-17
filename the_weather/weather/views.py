0c255a556420aa0e01d1c44ab1436c3daf1c4981
alexgithui91 <alexgithui91@gmail.com>
2021-11-17 09:18:46 +0300

    working API call. prints json successful

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,5 +1,22 @@
+import os
+import requests
 from django.shortcuts import render
+from dotenv import load_dotenv
+
+# Load API Key
+load_dotenv()
+api_key = os.environ.get("API_KEY")
 
 # Create your views here.
 def index(request):
+    url = (
+        "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid="
+        + api_key
+    )
+    city = "Las Vegas"
+
+    city_weather = requests.get(url.format(city)).json()
+
+    print(city_weather)
+
     return render(request, "weather/index.html")
