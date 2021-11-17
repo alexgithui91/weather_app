a6ff7107a3e4e60e46a4d8c49dbda04771539e15
alexgithui91 <alexgithui91@gmail.com>
2021-11-17 09:58:58 +0300

    able to loop and display all cities in db

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -2,6 +2,7 @@ import os
 import requests
 from django.shortcuts import render
 from dotenv import load_dotenv
+from .models import City
 
 # Load API Key
 load_dotenv()
@@ -13,17 +14,24 @@ def index(request):
         "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid="
         + api_key
     )
-    city = "Las Vegas"
 
-    city_weather = requests.get(url.format(city)).json()
+    cities = City.objects.all()  # return all the cities in the databse
 
-    weather = {
-        "city": city,
-        "temperature": city_weather["main"]["temp"],
-        "description": city_weather["weather"][0]["description"],
-        "icon": city_weather["weather"][0]["icon"],
-    }
+    weather_data = []
 
-    context = {"weather": weather}
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json()
+
+        weather = {
+            "city": city,
+            "temperature": city_weather["main"]["temp"],
+            "description": city_weather["weather"][0]["description"],
+            "icon": city_weather["weather"][0]["icon"],
+        }
+
+        weather_data.append(weather)
+
+    context = {"weather_data": weather_data}
 
     return render(request, "weather/index.html", context)
